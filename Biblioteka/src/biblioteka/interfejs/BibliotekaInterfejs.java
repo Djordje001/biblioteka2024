5f116bc258933f285dff8f314016097e2466808d
Djordje001 <djordjevic.dj01@gmail.com>
2024-03-04 12:46:04 +0100

    Napravljena klasa Biblioteka koja implementira Biblioteka interfejs

--- Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
+++ Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -9,5 +9,5 @@ public interface BibliotekaInterfejs {
 	public void dodajKnjigu(Knjiga k);
 	public void obrisiKnjigu(Knjiga k);
 	public List<Knjiga> vratiSveKnjige();
-	public List<Knjiga> pronadjiKnjig(Autor autor,long isbn,String naslov,String izdavac);
+	public List<Knjiga> pronadjiKnjigu(Autor autor,long isbn,String naslov,String izdavac);
 }
